0e9d66a8c1d00ac203c92d5a591d5d86a539b932
Коновалов Виталий <kremnilandk@gmail.com>
2022-11-18 15:04:36 +0300

    Paginator

--- acoffe/urls.py
+++ acoffe/urls.py
@@ -6,5 +6,9 @@ import acoffe.views as v
 urlpatterns = [
     path('', v.home_page, name='home_page'),
     path('list/', v.list_coffe, name='list_coffe'),
+
+    # ListView
+    # path('list/', v.CoffeList.as_view(), name='list_coffe'),
+
     path('list/<int:coffe_id>/', v.detail_coffe, name='detail_coffe'),
 ]
--- acoffe/views.py
+++ acoffe/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from acoffe.models import coffe
 
+from django.views.generic import ListView, DetailView
+
+from django.core.paginator import Paginator
+
 # Реализовать домашнюю страницу, вывода списка предлагаемых кофе и страницы с подробным описанием
 
 def home_page(request):
@@ -9,8 +13,14 @@ def home_page(request):
 
 def list_coffe(request):
     list_coffe = coffe.objects.all()
+    paginator = Paginator(list_coffe, 2)
+    page_num = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_num)
+
     context = {
         'list_coffe': list_coffe,
+        'page_obj': page_obj,
+        'title': 'Выбор кофе',
     }
     return render(request, 'acoffe/list_coffe.html', context)
 
@@ -20,3 +30,14 @@ def detail_coffe(reqest, coffe_id):
         'detail_coffe': detail_coffe,
     }
     return render(reqest, 'acoffe/detail_coffe.html', context)
+
+# class CoffeList(ListView):
+#     model = coffe
+#     template_name = 'acoffe/list_coffe.html'
+#     context_object_name = 'list_coffe'
+#     paginate_by = 2
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Выбор кофе'
+#         return context
